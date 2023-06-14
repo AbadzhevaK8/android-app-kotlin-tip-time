287f06195cfce21b604d59489d24bae22b1b9ad9
AbadzhevaK8 <make.me.loco@gmail.com>
2023-06-15 00:02:24 +0300

    feat: add material elements, styles, icons, landscape orientation.

--- app/src/main/java/android/abadzheva/tiptime/MainActivity.kt
+++ app/src/main/java/android/abadzheva/tiptime/MainActivity.kt
@@ -1,8 +1,12 @@
 package android.abadzheva.tiptime
 
 import android.abadzheva.tiptime.databinding.ActivityMainBinding
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null || cost == 0.0) {
             displayTip(0.0)
@@ -39,6 +43,17 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip : Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
 
