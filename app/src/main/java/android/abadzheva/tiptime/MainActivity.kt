51cdf671947e29cdd6155034b4c7ac3f11353b26
AbadzhevaK8 <make.me.loco@gmail.com>
2023-06-15 17:07:34 +0300

    feat: add instrumental tests.

--- app/src/main/java/android/abadzheva/tiptime/MainActivity.kt
+++ app/src/main/java/android/abadzheva/tiptime/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
     }
 
     private fun calculateTip() {
@@ -29,18 +36,19 @@ class MainActivity : AppCompatActivity() {
             return
         }
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
+            R.id.option_fifteen_percent -> 0.15
+            R.id.option_ten_percent -> 0.10
+            else -> 0.05
         }
         var tip = tipPercentage * cost
         if (binding.roundUpSwitch.isChecked) {
-            tip = kotlin.math.ceil(tip)
+            tip = ceil(tip)
         }
         NumberFormat.getCurrencyInstance()
         displayTip(tip)
     }
-    private fun displayTip(tip : Double) {
+
+    private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
